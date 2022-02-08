59e5d72593fc6515be1f63f5c82da155f885bdde
dianamariajaramillo <dimajao@hotmail.com>
2022-02-08 07:00:33 -0500

    :construction: email controller signup

--- src/main/java/co/edu/iudigital/app/controller/UsuarioController.java
+++ src/main/java/co/edu/iudigital/app/controller/UsuarioController.java
@@ -1,8 +1,12 @@
 package co.edu.iudigital.app.controller;
 
 import java.util.List;
+import java.util.Objects;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClientException;
 
 import co.edu.iudigital.app.dto.UsuarioDto;
 import co.edu.iudigital.app.exception.RestException;
 import co.edu.iudigital.app.model.Usuario;
 import co.edu.iudigital.app.service.iface.IEmailService;
 import co.edu.iudigital.app.service.iface.IUsuarioService;
+import co.edu.iudigital.app.util.ConstUtil;
 import co.edu.iudigital.app.util.Helper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -57,9 +61,27 @@ public class UsuarioController {
 		return ResponseEntity.ok().body(usuarioDto);
 	}
 	
+	@ApiOperation(value = "Da de alta a un usuario en la app",
+			response = Usuario.class,
+			produces = "application/json",
+			httpMethod = "POST")
 	@PostMapping("/signup")
-	public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) throws RestClientException{
-		
+	public ResponseEntity<UsuarioDto> create(@RequestBody @Valid Usuario usuario) throws RestException{
+		Usuario usuarioSaved = usuarioService.saveUser(usuario);		
+		// TODO: IMPLEMENTAR SPRING SECURITY
+		if(Objects.nonNull(usuarioSaved)) {
+			String mess = "Su usuario "+ usuarioSaved.getUsername() +
+					" y contraseña " + usuarioSaved.getPassword();
+			String to = usuarioSaved.getUsername();
+			String subj = ConstUtil.ASUNTO_MESSAGE;
+			boolean sent = emailService.sendEmail(mess, to, subj);
+			if(!sent) {
+				System.out.print("No envió mensaje");
+				// TODO: Colocar log y un exception
+			}
+		}
+		UsuarioDto usuarioDto = Helper.getMapValuesClient(usuarioSaved);
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioDto);
 	}
 	
 }
--- src/main/java/co/edu/iudigital/app/util/ConstUtil.java
+++ src/main/java/co/edu/iudigital/app/util/ConstUtil.java
@@ -2,6 +2,9 @@ package co.edu.iudigital.app.util;
 
 public interface ConstUtil {
 	
-String MESSAGE_NOT_FOUND = "No encontró el prospecto";
+	String MESSAGE_NOT_FOUND = "No encontró el prospecto";
+
+	// EMAIL
+	String ASUNTO_MESSAGE = "Registro en HelpmeIUD";
 
 }
